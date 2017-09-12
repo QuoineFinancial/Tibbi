3faad1f83e943371e7acc42aa66f382118f2f562
Cristian Azov <cristian.azov@mttnow.com>
2017-09-12 13:56:27 +0300

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-Tibbi_Tests/Pods-Tibbi_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tibbi_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Tibbi_Tests
+@end
---
+++ Tibbi/Classes/UITableView+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  UITableView+Extensions.swift
+//  Viper
+//
+//  Created by Cristian Azov on 8/23/17.
+//  Copyright © 2017 Cristian Azov. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    /// Assumes that NibNames match CellIdentifiers and ClassNames
+    
+    open func registerNibs(for cellClasses: [AnyClass]) {
+        for cellClass in cellClasses {
+            self.registerNib(for:cellClass.self)
+        }
+    }
+    
+    /// Assumes that NibName matches CellIdentifier and ClassName
+    
+    open func registerNib(for cellClass: AnyClass) {
+        self.registerNib(name: String(describing: cellClass.self), cellClass: cellClass)
+    }
+    
+    /// Assumes that NibName matches CellIdentifier
+    
+    open func registerNib(name: String, cellClass: AnyClass) {
+        let nib = UINib(nibName: name, bundle: Bundle(for: cellClass))
+        self.register(nib, forCellReuseIdentifier: String(describing: cellClass.self))
+    }
+}
